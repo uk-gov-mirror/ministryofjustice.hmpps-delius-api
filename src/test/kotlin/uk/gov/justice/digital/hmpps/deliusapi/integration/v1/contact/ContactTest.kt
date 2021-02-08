d9e7ad108cacb2e6f8827bc67b8fadc01ce6b6ab
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-02-08 14:21:39 +0000

    better integration test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/deliusapi/integration/v1/contact/ContactTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/deliusapi/integration/v1/contact/ContactTest.kt
@@ -1,14 +1,20 @@
 package uk.gov.justice.digital.hmpps.deliusapi.integration.v1.contact
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.deliusapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.deliusapi.integration.JwtAuthHelper
+import uk.gov.justice.digital.hmpps.deliusapi.repository.ContactRepository
 import uk.gov.justice.digital.hmpps.deliusapi.util.Fake
 
 class ContactTest : IntegrationTestBase() {
   @Autowired
   private lateinit var jwtAuthHelper: JwtAuthHelper
+
+  @Autowired
+  private lateinit var repository: ContactRepository
 
   @Test
   fun `Attempting to create contact without authentication`() {
@@ -41,9 +47,16 @@ class ContactTest : IntegrationTestBase() {
     webTestClient.post()
       .uri("/v1/contact")
       .header("Authorization", "Bearer $token")
+      .contentType(MediaType.APPLICATION_JSON)
+      .accept(MediaType.APPLICATION_JSON)
       .bodyValue(newContact)
       .exchange()
       .expectStatus()
       .isOk
+      .expectBody()
+      .jsonPath("$.id").value<Long> {
+        Assertions.assertThat(it).isPositive
+        Assertions.assertThat(repository.existsById(it)).describedAs("should save contact").isTrue
+      }
   }
 }

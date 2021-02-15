09adaeedf44f0d89c1f953dcca31ebef6ae02794
marcus.aspin <maspin@unilink.com>
2021-02-15 11:13:06 +0000

    Re-expand imports to appease the linter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/entity/ContactType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/entity/ContactType.kt
@@ -2,7 +2,13 @@ package uk.gov.justice.digital.hmpps.deliusapi.entity
 
 import org.hibernate.annotations.Type
 import org.hibernate.annotations.Where
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
+import javax.persistence.Table
 
 @Entity
 @Table(name = "R_CONTACT_TYPE")

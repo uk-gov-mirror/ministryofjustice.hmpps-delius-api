f3433c8d465a1130c3f8812679a3e910a22bbac4
marcus.aspin <maspin@unilink.com>
2021-02-24 09:26:43 +0000

    Fix lint issues

--- src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/controller/v1/ContactController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/controller/v1/ContactController.kt
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.created
 import org.springframework.http.ResponseEntity.status
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody

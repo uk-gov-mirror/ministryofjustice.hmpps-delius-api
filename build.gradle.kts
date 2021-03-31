2414853e1cff2b746582fe3bc1abdd7439301456
Ed Brindley <ed@maidavale.org>
2021-03-31 15:59:26 +0100

    DAPI-154 Update dependencies to resolve OWASP vulnerability check failure (#89)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
-  kotlin("plugin.jpa") version "1.4.30"
-  kotlin("kapt") version "1.4.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.3"
+  kotlin("plugin.spring") version "1.4.32"
+  kotlin("plugin.jpa") version "1.4.32"
+  kotlin("kapt") version "1.4.32"
 }
 
 configurations {
@@ -18,13 +18,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("io.springfox:springfox-boot-starter:3.0.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.4")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
   implementation("com.github.java-json-tools:json-patch:1.13")
 
-  implementation("org.flywaydb:flyway-core:7.5.2")
-  implementation("com.zaxxer:HikariCP:4.0.1")
+  implementation("org.flywaydb:flyway-core:7.7.1")
+  implementation("com.zaxxer:HikariCP:4.0.3")
   implementation("com.h2database:h2")
-  implementation("com.oracle.database.jdbc:ojdbc10:19.9.0.0")
+  implementation("com.oracle.database.jdbc:ojdbc10:19.10.0.0")
 
   implementation("org.mapstruct:mapstruct:1.4.2.Final")
   kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
